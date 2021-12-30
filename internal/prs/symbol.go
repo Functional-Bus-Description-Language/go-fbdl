7f4fdc937d27f5c6eaed959134c0bf5481286c23
Michał Kruszewski <mkru@protonmail.com>
2021-12-30 15:39:48 +0100

    Minor changes in prs/symbol.go.

--- internal/prs/symbol.go
+++ internal/prs/symbol.go
@@ -17,11 +17,18 @@ type base struct {
 	parent     Searchable
 }
 
-func (b base) Name() string        { return b.name }
-func (b base) LineNumber() uint32  { return b.lineNumber }
-func (b *base) SetParent(s Symbol) { b.parent = s }
-func (b base) Parent() Searchable  { return b.parent }
-func (b *base) File() *File        { return b.file }
+func (b base) Name() string       { return b.name }
+func (b base) LineNumber() uint32 { return b.lineNumber }
+func (b base) Parent() Searchable { return b.parent }
+func (b base) File() *File        { return b.file }
+
+func (b *base) SetParent(s Symbol) {
+	if b.parent != nil {
+		panic("should never happen")
+	}
+
+	b.parent = s
+}
 
 func (b *base) SetFile(f *File) {
 	if b.file != nil {
