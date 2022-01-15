88de8e18313d871db71c4bc3c8ba36f286d2b3db
Michał Kruszewski <mkru@protonmail.com>
2022-01-15 10:03:09 +0100

    Add support for int list constant in package.

--- pkg/fbdl/pkg.go
+++ pkg/fbdl/pkg.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Package struct {
-	IntConsts map[string]int64
-	StrConsts map[string]string
+	IntConsts     map[string]int64
+	IntListConsts map[string][]int64
+	StrConsts     map[string]string
 }
 
 func (p Package) HasConsts() bool {
-	if len(p.IntConsts) != 0 {
+	if len(p.IntConsts) != 0 || len(p.IntListConsts) != 0 {
 		return true
 	}
 	if len(p.StrConsts) != 0 {
@@ -30,34 +31,69 @@ func ConstifyPackages(packages prs.Packages) map[string]Package {
 			panic("not yet implemented")
 		}
 		for _, pkg := range pkgs {
-			cPkgs[name] = Package{
-				IntConsts: map[string]int64{},
-				StrConsts: map[string]string{},
-			}
-			for _, s := range pkg.Symbols {
-				if c, ok := s.(*prs.Const); ok {
-					v, err := c.Value.Eval()
-					if err != nil {
-						panic("not yet implemented")
-					}
-					switch v.(type) {
-					case val.BitStr:
-						panic("not yet implemented")
-					case val.Bool:
-						panic("not yet implemented")
-					case val.Int:
-						cPkgs[name].IntConsts[c.Name()] = int64(v.(val.Int))
-					case val.List:
-						panic("not yet implemented")
-					case val.Str:
-						cPkgs[name].StrConsts[c.Name()] = string(v.(val.Str))
-					default:
-						panic("should never happen")
-					}
-				}
-			}
+			cPkgs[name] = constifyPkg(pkg)
 		}
 	}
 
 	return cPkgs
+}
+
+func constifyPkg(pkg *prs.Package) Package {
+	p := Package{
+		IntConsts:     map[string]int64{},
+		IntListConsts: map[string][]int64{},
+		StrConsts:     map[string]string{},
+	}
+
+	for _, s := range pkg.Symbols {
+		if c, ok := s.(*prs.Const); ok {
+			v, err := c.Value.Eval()
+			if err != nil {
+				panic("not yet implemented")
+			}
+			switch v.(type) {
+			case val.BitStr:
+				panic("not yet implemented")
+			case val.Bool:
+				panic("not yet implemented")
+			case val.Int:
+				p.IntConsts[c.Name()] = int64(v.(val.Int))
+			case val.List:
+				switch v.(val.List)[0].(type) {
+				case val.BitStr:
+					panic("not yet implemented")
+				case val.Bool:
+					panic("not yet implemented")
+				case val.Int:
+					p.IntListConsts[c.Name()] = constifyIntList(v.(val.List))
+				case val.Str:
+					panic("not yet implemented")
+				default:
+					panic("should never happen")
+				}
+			case val.Str:
+				p.StrConsts[c.Name()] = string(v.(val.Str))
+			default:
+				panic("should never happen")
+			}
+		}
+	}
+
+	return p
+}
+
+// constifyIntList tries to constify list as an int list.
+// If any elemnt is of different type than val.Int, then it returns nil.
+func constifyIntList(l val.List) []int64 {
+	ints := []int64{}
+
+	for _, v := range l {
+		if i, ok := v.(val.Int); ok {
+			ints = append(ints, int64(i))
+		} else {
+			return nil
+		}
+	}
+
+	return ints
 }
