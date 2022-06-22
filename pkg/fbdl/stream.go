43843f777bb2a5215bd47cdf06cf04dcf5e53b70
Michał Kruszewski <mkru@protonmail.com>
2022-06-22 21:37:10 +0200

    Add support for upstream registerification.

--- pkg/fbdl/stream.go
+++ pkg/fbdl/stream.go
@@ -2,6 +2,8 @@ package fbdl
 
 import (
 	"github.com/Functional-Bus-Description-Language/go-fbdl/internal/ins"
+	"github.com/Functional-Bus-Description-Language/go-fbdl/internal/val"
+	"github.com/Functional-Bus-Description-Language/go-fbdl/pkg/fbdl/access"
 )
 
 // Stream represents stream element.
@@ -57,6 +59,8 @@ func regStream(insStream *ins.Element, addr int64) (*Stream, int64) {
 
 	if len(params) == 0 && len(returns) == 0 {
 		return regEmptyStream(&stream, addr)
+	} else if len(returns) > 0 {
+		return regUpstream(&stream, addr, returns)
 	} else {
 		panic("not yet supported")
 	}
@@ -67,4 +71,42 @@ func regStream(insStream *ins.Element, addr int64) (*Stream, int64) {
 func regEmptyStream(stream *Stream, addr int64) (*Stream, int64) {
 	stream.StbAddr = addr
 	return stream, addr + 1
+}
+
+func regUpstream(stream *Stream, addr int64, returns []*ins.Element) (*Stream, int64) {
+	baseBit := int64(0)
+	for _, ret := range returns {
+		r := Return{
+			Name:    ret.Name,
+			Doc:     ret.Doc,
+			IsArray: ret.IsArray,
+			Count:   ret.Count,
+			Width:   int64(ret.Props["width"].(val.Int)),
+		}
+
+		if r.IsArray {
+			r.Access = access.MakeArrayContinuous(r.Count, addr, baseBit, r.Width)
+		} else {
+			r.Access = access.MakeSingle(addr, baseBit, r.Width)
+		}
+
+		if r.Access.EndBit() < busWidth-1 {
+			addr += r.Access.RegCount() - 1
+			baseBit = r.Access.EndBit() + 1
+		} else {
+			addr += r.Access.RegCount()
+			baseBit = 0
+		}
+
+		stream.Returns = append(stream.Returns, &r)
+	}
+
+	stream.AckAddr = stream.Returns[len(stream.Returns)-1].Access.EndAddr()
+
+	lastAccess := stream.Returns[len(stream.Returns)-1].Access
+	if lastAccess.EndBit() < busWidth-1 {
+		addr += 1
+	}
+
+	return stream, addr
 }
