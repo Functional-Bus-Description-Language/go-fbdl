f76544bcb00fe7a58df6089c7e94cbbfcade79b3
Michał Kruszewski <mkru@protonmail.com>
2022-07-01 18:00:09 +0200

    Minor refactoring in status registerification.

--- internal/reg/status.go
+++ internal/reg/status.go
@@ -3,7 +3,6 @@ package reg
 import (
 	"github.com/Functional-Bus-Description-Language/go-fbdl/internal/elem"
 	"github.com/Functional-Bus-Description-Language/go-fbdl/internal/gap"
-	_ "github.com/Functional-Bus-Description-Language/go-fbdl/internal/val"
 	"github.com/Functional-Bus-Description-Language/go-fbdl/pkg/fbdl/access"
 )
 
@@ -17,43 +16,53 @@ func regStatus(st *elem.Status, addr int64, gp *gap.Pool) int64 {
 }
 
 func regStatusSingle(st *elem.Status, addr int64, gp *gap.Pool) int64 {
+	var a access.Access
+
 	if g, ok := gp.GetSingle(st.Width(), false); ok {
-		st.SetAccess(access.MakeSingleSingle(g.EndAddr, g.StartBit(), st.Width()))
+		a = access.MakeSingleSingle(g.EndAddr, g.StartBit(), st.Width())
 	} else {
-		st.SetAccess(access.MakeSingle(addr, 0, st.Width()))
-		addr += st.Access().RegCount()
+		a = access.MakeSingle(addr, 0, st.Width())
+		addr += a.RegCount()
 	}
-	if st.Access().EndBit() < busWidth-1 {
+	if a.EndBit() < busWidth-1 {
 		gp.Add(gap.Gap{
-			StartAddr: st.Access().EndAddr(),
-			EndAddr:   st.Access().EndAddr(),
-			Mask:      access.Mask{Upper: busWidth - 1, Lower: st.Access().EndBit() + 1},
+			StartAddr: a.EndAddr(),
+			EndAddr:   a.EndAddr(),
+			Mask:      access.Mask{Upper: busWidth - 1, Lower: a.EndBit() + 1},
 			WriteSafe: true,
 		})
 	}
+
+	st.SetAccess(a)
 
 	return addr
 }
 
 func regStatusArray(st *elem.Status, addr int64, gp *gap.Pool) int64 {
+	var a access.Access
+
 	if busWidth/2 < st.Width() && st.Width() <= busWidth {
-		st.SetAccess(access.MakeArraySingle(st.Count(), addr, 0, st.Width()))
+		a = access.MakeArraySingle(st.Count(), addr, 0, st.Width())
 		// TODO: This is a place for adding a potential Gap.
 	} else if busWidth%st.Width() == 0 || st.Count() <= busWidth/st.Width() || st.Width() < busWidth/2 {
-		st.SetAccess(access.MakeArrayMultiplePacked(st.Count(), addr, st.Width()))
+		a = access.MakeArrayMultiplePacked(st.Count(), addr, st.Width())
 		// TODO: This is a place for adding a potential Gap.
 	} else {
 		panic("not yet implemented")
 	}
-	addr += st.Access().RegCount()
+	addr += a.RegCount()
+
+	st.SetAccess(a)
 
 	return addr
 }
 
 func regStatusArraySingle(st *elem.Status, addr, startBit int64) int64 {
-	st.SetAccess(access.MakeArraySingle(st.Count(), addr, startBit, st.Width()))
+	a := access.MakeArraySingle(st.Count(), addr, startBit, st.Width())
 
-	addr += st.Access().RegCount()
+	addr += a.RegCount()
+
+	st.SetAccess(a)
 
 	return addr
 }
