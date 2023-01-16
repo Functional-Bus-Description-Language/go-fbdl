b12bd90178cec14aff6e1b20e1afd601924bf10b
Michał Kruszewski <mkru@protonmail.com>
2023-01-16 10:08:40 +0100

    Add ReturnsBufSize and ReturnsStartAddr functions

--- pkg/fbdl/elem/proc.go
+++ pkg/fbdl/elem/proc.go
@@ -1,6 +1,7 @@
 package elem
 
 import (
+	"fmt"
 	"github.com/Functional-Bus-Description-Language/go-fbdl/pkg/fbdl/val"
 )
 
@@ -27,10 +28,35 @@ func (p *Proc) ParamsBufSize() int64 {
 	return params[l-1].Access.EndAddr() - params[0].Access.StartAddr() + 1
 }
 
+// ParamsStartAddr panics if proc has no params.
 func (p *Proc) ParamsStartAddr() int64 {
 	if len(p.Params) == 0 {
-		return p.CallAddr
+		panic(
+			fmt.Sprintf("proc %s has no params", p.Name),
+		)
 	}
 
 	return p.Params[0].Access.StartAddr()
+}
+
+func (p *Proc) ReturnsBufSize() int64 {
+	rets := p.Returns
+	l := len(rets)
+
+	if l == 0 {
+		return 0
+	}
+
+	return rets[l-1].Access.EndAddr() - rets[0].Access.StartAddr() + 1
+}
+
+// ReturnsStartAddr panics if proc has no returns.
+func (p *Proc) ReturnsStartAddr() int64 {
+	if len(p.Returns) == 0 {
+		panic(
+			fmt.Sprintf("proc %s has no returns", p.Name),
+		)
+	}
+
+	return p.Returns[0].Access.StartAddr()
 }
