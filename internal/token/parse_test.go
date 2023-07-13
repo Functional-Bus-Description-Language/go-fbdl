6b0bf6af9cd3f50208443606e146addff7705a90
Michał Kruszewski <mkru@protonmail.com>
2023-07-13 12:00:09 +0200

    Add test index

--- internal/token/parse_test.go
+++ internal/token/parse_test.go
@@ -7,30 +7,35 @@ import (
 
 func TestParse(t *testing.T) {
 	var tests = []struct {
+		idx  int // Test index, useful for navigation
 		src  string
 		want Stream
 	}{
-		{ // 0
+		{
+			0,
 			"\n\n",
 			Stream{
 				Token{Kind: NEWLINE, Pos: Position{Start: 0, End: 0, Line: 1, Column: 1}},
 				Token{Kind: NEWLINE, Pos: Position{Start: 1, End: 1, Line: 2, Column: 1}},
 			},
 		},
-		{ // 1
+		{
+			1,
 			"# Comment line",
 			Stream{
 				Token{Kind: COMMENT, Pos: Position{Start: 0, End: 13, Line: 1, Column: 1}},
 			},
 		},
-		{ // 2
+		{
+			2,
 			"# Comment\n",
 			Stream{
 				Token{Kind: COMMENT, Pos: Position{Start: 0, End: 8, Line: 1, Column: 1}},
 				Token{Kind: NEWLINE, Pos: Position{Start: 9, End: 9, Line: 1, Column: 10}},
 			},
 		},
-		{ // 3
+		{
+			3,
 			"# Comment 1\n# Comment 2",
 			Stream{
 				Token{Kind: COMMENT, Pos: Position{Start: 0, End: 10, Line: 1, Column: 1}},
@@ -38,7 +43,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: COMMENT, Pos: Position{Start: 12, End: 22, Line: 2, Column: 1}},
 			},
 		},
-		{ // 4
+		{
+			4,
 			"const A = true",
 			Stream{
 				Token{Kind: CONST, Pos: Position{Start: 0, End: 4, Line: 1, Column: 1}},
@@ -47,7 +53,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: BOOL, Pos: Position{Start: 10, End: 13, Line: 1, Column: 11}},
 			},
 		},
-		{ // 5
+		{
+			5,
 			"foo mask; atomic = false",
 			Stream{
 				Token{Kind: IDENT, Pos: Position{Start: 0, End: 2, Line: 1, Column: 1}},
@@ -58,7 +65,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: BOOL, Pos: Position{Start: 19, End: 23, Line: 1, Column: 20}},
 			},
 		},
-		{ // 6
+		{
+			6,
 			"i irq; add-enable = true",
 			Stream{
 				Token{Kind: IDENT, Pos: Position{Start: 0, End: 0, Line: 1, Column: 1}},
@@ -69,7 +77,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: BOOL, Pos: Position{Start: 20, End: 23, Line: 1, Column: 21}},
 			},
 		},
-		{ // 7
+		{
+			7,
 			"type cfg_t(w = 10) config; width = w",
 			Stream{
 				Token{Kind: TYPE, Pos: Position{Start: 0, End: 3, Line: 1, Column: 1}},
@@ -86,7 +95,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: IDENT, Pos: Position{Start: 35, End: 35, Line: 1, Column: 36}},
 			},
 		},
-		{ // 8
+		{
+			8,
 			"s static; groups = [\"a\", \"b\"]",
 			Stream{
 				Token{Kind: IDENT, Pos: Position{Start: 0, End: 0, Line: 1, Column: 1}},
@@ -101,7 +111,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: RBRACK, Pos: Position{Start: 28, End: 28, Line: 1, Column: 29}},
 			},
 		},
-		{ // 9
+		{
+			9,
 			"import foo \"path\"",
 			Stream{
 				Token{Kind: IMPORT, Pos: Position{Start: 0, End: 5, Line: 1, Column: 1}},
@@ -109,7 +120,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: STRING, Pos: Position{Start: 11, End: 16, Line: 1, Column: 12}},
 			},
 		},
-		{ // 10
+		{
+			10,
 			"const A = 2**5 - 1",
 			Stream{
 				Token{Kind: CONST, Pos: Position{Start: 0, End: 4, Line: 1, Column: 1}},
@@ -122,7 +134,8 @@ func TestParse(t *testing.T) {
 				Token{Kind: INT, Pos: Position{Start: 17, End: 17, Line: 1, Column: 18}},
 			},
 		},
-		{ // 11
+		{
+			11,
 			"const A1 = 0b1 << 0o3",
 			Stream{
 				Token{Kind: CONST, Pos: Position{Start: 0, End: 4, Line: 1, Column: 1}},
@@ -136,6 +149,10 @@ func TestParse(t *testing.T) {
 	}
 
 	for i, test := range tests {
+		if i != test.idx {
+			t.Fatalf("Invalid test index %d, expecting %d", test.idx, i)
+		}
+
 		got, err := Parse([]byte(test.src))
 		if err != nil {
 			t.Fatalf("Test %d: error != nil: %v", i, err)
@@ -161,57 +178,77 @@ func TestParse(t *testing.T) {
 
 func TestParseError(t *testing.T) {
 	var tests = []struct {
+		idx int // Test index, useful for navigation
 		src string
 		err error
 	}{
-		{ // 0
+		{
+			0,
 			"\n ", fmt.Errorf("2:1: space character ' ' not allowed for indent"),
 		},
-		{ // 1
+		{
+			1,
 			";\n", fmt.Errorf("1:1: extra ';' at the end of line"),
 		},
-		{ // 2
+		{
+			2,
 			" ; \n", fmt.Errorf("1:2: extra ';' at the end of line"),
 		},
-		{ // 3
+		{
+			3,
 			";;", fmt.Errorf("1:2: redundant ';'"),
 		},
-		{ // 4
+		{
+			4,
 			"b\"01-uUwWxXzZ3\"", fmt.Errorf("1:14: invalid character '3' in binary bit string literal"),
 		},
-		{ // 5
+		{
+			5,
 			"B\"0", fmt.Errorf("1:1: missing terminating '\"' in binary bit string literal"),
 		},
-		{ // 6
+		{
+			6,
 			"o\"01234567-uUwWxXzZ8\"", fmt.Errorf("1:20: invalid character '8' in octal bit string literal"),
 		},
-		{ // 7
+		{
+			7,
 			"O\"0", fmt.Errorf("1:1: missing terminating '\"' in octal bit string literal"),
 		},
-		{ // 8
+		{
+			8,
 			"x\"0123456789aAbBcCdDeEfF-uUwWxXzZ8g\"", fmt.Errorf("1:35: invalid character 'g' in hex bit string literal"),
 		},
-		{ // 9
+		{
+			9,
 			"X\"0", fmt.Errorf("1:1: missing terminating '\"' in hex bit string literal"),
 		},
-		{ // 10
+		{
+			10,
 			",,", fmt.Errorf("1:2: redundant ','"),
 		},
-		{ // 11
+		{
+			11,
 			"1.2.3", fmt.Errorf("1:4: second point character '.' in number literal"),
 		},
-		{ // 12
+		{
+			12,
 			"1e2.", fmt.Errorf("1:4: point character '.' after exponent in number literal"),
 		},
-		{ // 13
+		{
+			13,
 			"1e2d", fmt.Errorf("1:4: invalid character 'd' in number literal"),
 		},
-		{ // 14
+		{
+			14,
 			"\n\"str", fmt.Errorf("2:1: unterminated string literal"),
 		},
 	}
 
 	for i, test := range tests {
+		if i != test.idx {
+			t.Fatalf("Invalid test index %d, expecting %d", test.idx, i)
+		}
+
 		_, err := Parse([]byte(test.src))
 		if err == nil {
 			t.Fatalf("%d: err = nil, expecting != nil", i)
