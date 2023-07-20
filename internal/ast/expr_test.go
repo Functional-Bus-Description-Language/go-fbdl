bde212ee20d401eb06ff60d55a0ae178b3e620d8
Michał Kruszewski <mkru@protonmail.com>
2023-07-20 13:21:40 +0200

    ast: Add one more UnaryExpr test

--- internal/ast/expr_test.go
+++ internal/ast/expr_test.go
@@ -6,10 +6,7 @@ import (
 )
 
 func TestIdentExpr(t *testing.T) {
-	toks, err := token.Parse([]byte("id"))
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
+	toks, _ := token.Parse([]byte("id"))
 	want := Ident{Name: toks[0]}
 
 	i, got, err := buildExpr(toks, 0)
@@ -25,15 +22,28 @@ func TestIdentExpr(t *testing.T) {
 }
 
 func TestUnaryExpr(t *testing.T) {
-	toks, err := token.Parse([]byte("-abc"))
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
+	toks, _ := token.Parse([]byte("-abc"))
 	want := UnaryExpr{
 		Op: toks[0], X: Ident{Name: toks[1]},
 	}
 
 	i, got, err := buildExpr(toks, 0)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	if i != 2 {
+		t.Fatalf("i = %d", i)
+	}
+	if got != want {
+		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
+	}
+
+	toks, _ = token.Parse([]byte("+ 10"))
+	want = UnaryExpr{
+		Op: toks[0], X: Int{Val: toks[1].(token.Int)},
+	}
+
+	i, got, err = buildExpr(toks, 0)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
