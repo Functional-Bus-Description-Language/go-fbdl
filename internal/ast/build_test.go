b16d2e4b70e3ead1cd9bc50200b7c32347bfb7a9
Michał Kruszewski <mkru@protonmail.com>
2023-07-24 09:17:05 +0200

    ast: Make test code slightly less verbose

--- internal/ast/build_test.go
+++ internal/ast/build_test.go
@@ -1,23 +1,38 @@
 package ast
 
 import (
+	"fmt"
 	"github.com/Functional-Bus-Description-Language/go-fbdl/internal/token"
 	"testing"
 )
 
+func check(i int, wantI int, got Expr, want Expr, err error) error {
+	if err != nil {
+		return err
+	}
+
+	errMsg := "i = %d, expected i = %d\n\ngot:  %+v\nwant: %+v"
+	switch want := want.(type) {
+	case CallExpr:
+		if i != wantI || !want.eq(got.(CallExpr)) {
+			return fmt.Errorf(errMsg, i, wantI, got, want)
+		}
+	default:
+		if i != wantI || got != want {
+			return fmt.Errorf(errMsg, i, wantI, got, want)
+		}
+	}
+
+	return nil
+}
+
 func TestBuildIdent(t *testing.T) {
 	toks, _ := token.Parse([]byte("id"))
 	want := Ident{Name: toks[0]}
-
 	i, got, err := buildExpr(toks, 0, nil)
+	err = check(i, 1, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 1 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 }
 
@@ -26,32 +41,20 @@ func TestBuildUnaryExpr(t *testing.T) {
 	want := UnaryExpr{
 		Op: toks[0], X: Ident{Name: toks[1]},
 	}
-
 	i, got, err := buildExpr(toks, 0, nil)
+	err = check(i, 2, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 2 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 
 	toks, _ = token.Parse([]byte("+ 10"))
 	want = UnaryExpr{
 		Op: toks[0], X: Int{Val: toks[1].(token.Int)},
 	}
-
 	i, got, err = buildExpr(toks, 0, nil)
+	err = check(i, 2, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 2 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 }
 
@@ -65,16 +68,10 @@ func TestBuildCallExpr(t *testing.T) {
 		},
 		Rparen: toks[3].(token.RightParen),
 	}
-
 	i, got, err := buildExpr(toks, 0, nil)
+	err = check(i, 4, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 4 {
-		t.Fatalf("i = %d", i)
-	}
-	if !want.eq(got.(CallExpr)) {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 
 	toks, _ = token.Parse([]byte("foo(12.35, true)"))
@@ -87,16 +84,10 @@ func TestBuildCallExpr(t *testing.T) {
 		},
 		Rparen: toks[5].(token.RightParen),
 	}
-
 	i, got, err = buildExpr(toks, 0, nil)
+	err = check(i, 6, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 6 {
-		t.Fatalf("i = %d", i)
-	}
-	if !want.eq(got.(CallExpr)) {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 }
 
@@ -115,7 +106,6 @@ func TestBuildBinaryExpr(t *testing.T) {
 	if got != want {
 		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
-
 	toks, _ = token.Parse([]byte("A + B * C"))
 	want = BinaryExpr{
 		X:  Ident{Name: toks[0]},
@@ -127,14 +117,9 @@ func TestBuildBinaryExpr(t *testing.T) {
 		},
 	}
 	i, got, err = buildExpr(toks, 0, nil)
+	err = check(i, 5, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 5 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 
 	toks, _ = token.Parse([]byte("A * B - C"))
@@ -148,14 +133,9 @@ func TestBuildBinaryExpr(t *testing.T) {
 		Y:  Ident{Name: toks[4]},
 	}
 	i, got, err = buildExpr(toks, 0, nil)
+	err = check(i, 5, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 5 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 
 	toks, _ = token.Parse([]byte("A ** B + C / D"))
@@ -173,13 +153,8 @@ func TestBuildBinaryExpr(t *testing.T) {
 		},
 	}
 	i, got, err = buildExpr(toks, 0, nil)
+	err = check(i, 7, got, want, err)
 	if err != nil {
 		t.Fatalf("%v", err)
-	}
-	if i != 7 {
-		t.Fatalf("i = %d", i)
-	}
-	if got != want {
-		t.Fatalf("\ngot:  %+v\nwant: %+v", got, want)
 	}
 }
