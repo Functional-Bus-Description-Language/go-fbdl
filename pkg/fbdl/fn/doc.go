f82dddceb1f4c950f7168f968f4b403f7e4e9542
Michał Kruszewski <mkru@protonmail.com>
2023-08-25 14:39:53 +0200

    fn: Update doc

--- pkg/fbdl/fn/doc.go
+++ pkg/fbdl/fn/doc.go
@@ -1,4 +1,2 @@
 // Package fbdl/func implements FBDL functionality types.
-// Functions operating on Functionality interface are implemented as package functions.
-// Functions specific for particular types are implemented as type methods.
 package fn
