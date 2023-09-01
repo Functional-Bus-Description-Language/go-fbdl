05351a6687c7f61525ce5dc10eb667ea12359309
Michał Kruszewski <mkru@protonmail.com>
2023-09-01 11:46:45 +0200

    Improve some error messages

--- internal/prs/check.go
+++ internal/prs/check.go
@@ -5,17 +5,17 @@ import (
 )
 
 func checkPropConflict(typ string, prop Prop, props PropContainer) error {
-	msg := `cannot set '%s' property, because '%s' property is already set in line %d`
+	msg := `cannot set '%s' property, because '%s' property is already set in line %d column %d`
 
 	if w, ok := props.Get("width"); ok {
 		if prop.Name == "range" {
-			return fmt.Errorf(msg, "range", "width", w.Line)
+			return fmt.Errorf(msg, "range", "width", w.Line, w.Col)
 		}
 	}
 
 	if r, ok := props.Get("range"); ok {
 		if prop.Name == "width" {
-			return fmt.Errorf(msg, "width", "range", r.Line)
+			return fmt.Errorf(msg, "width", "range", r.Line, r.Col)
 		}
 	}
 
