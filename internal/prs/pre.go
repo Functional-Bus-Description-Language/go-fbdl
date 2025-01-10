3e8d8026c534c22c5d6d5711078ea1620949e4f9
Michał Kruszewski <mkru@protonmail.com>
2025-01-10 19:04:17 +0100

    Adjust package discovery according to the specification

--- internal/prs/pre.go
+++ internal/prs/pre.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,18 +17,10 @@ func DiscoverPackages(main string) Packages {
 	}
 	pathsToLook = append(pathsToLook, cwd)
 
-	cwdfbd := path.Join(cwd, "fbd")
-	_, err = os.Stat(cwdfbd)
-	if err == nil {
-		pathsToLook = append(pathsToLook, cwdfbd)
-	}
-
 	fbdpath := os.Getenv("FBDPATH")
 	if len(fbdpath) != 0 {
 		pathsToLook = append(pathsToLook, strings.Split(fbdpath, string(os.PathListSeparator))...)
 	}
-
-	// TODO: Add support for $HOME/local/.lib/fbd
 
 	dbgMsg := fmt.Sprintf("debug: looking for packages in following %d directories:\n", len(pathsToLook))
 	for _, path := range pathsToLook {
@@ -38,47 +30,10 @@ func DiscoverPackages(main string) Packages {
 
 	packages := make(Packages)
 
-	for _, checkPath := range pathsToLook {
-		content, err := os.ReadDir(checkPath)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, c := range content {
-			pkgPath := path.Join(checkPath, c.Name())
-
-			if !c.IsDir() {
-				fileInfo, err := os.Lstat(pkgPath)
-				if err != nil {
-					panic(err)
-				}
-				if fileInfo.Mode()&os.ModeSymlink != 0 {
-					pointee, err := os.Readlink(path.Join(checkPath, c.Name()))
-					if err != nil {
-						panic(err)
-					}
-					pkgPath = path.Join(checkPath, pointee)
-				} else {
-					continue
-				}
-			}
-
-			pkgName := strings.TrimPrefix(c.Name(), "fbd-")
-			innerContent, err := os.ReadDir(path.Join(checkPath, c.Name()))
-			if err != nil {
-				panic(err)
-			}
-			for _, ic := range innerContent {
-				if ic.IsDir() {
-					continue
-				}
-				fileName := ic.Name()
-				if strings.HasSuffix(fileName, ".fbd") {
-					pkg := Package{Name: pkgName, Path: pkgPath}
-					packages[pkgName] = append(packages[pkgName], &pkg)
-					break
-				}
-			}
+	for _, path := range pathsToLook {
+		pkgs := checkDir(path)
+		for name, ps := range pkgs {
+			packages[name] = append(packages[name], ps...)
 		}
 	}
 
@@ -99,6 +54,64 @@ func DiscoverPackages(main string) Packages {
 		}
 	}
 	log.Print(dbgMsg)
+
+	return packages
+}
+
+func checkDir(dirPath string) Packages {
+	packages := make(Packages)
+
+	dirEntires, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	base := filepath.Base(dirPath)
+	pkgPath := dirPath
+	hasPkgPrefix := strings.HasPrefix(base, "fbd-")
+	isPkgDir := false
+
+	for _, de := range dirEntires {
+		dePath := filepath.Join(dirPath, de.Name())
+		fileInfo, err := os.Lstat(dePath)
+		if err != nil {
+			panic(err)
+		}
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			dePath, err = filepath.EvalSymlinks(dePath)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		fileInfo, err = os.Stat(dePath)
+		if err != nil {
+			panic(err)
+		}
+
+		if fileInfo.IsDir() {
+			pkgs := checkDir(dePath)
+			for name, ps := range pkgs {
+				packages[name] = append(packages[name], ps...)
+			}
+			continue
+		}
+
+		if !hasPkgPrefix || isPkgDir {
+			continue
+		}
+
+		fileName := de.Name()
+		if strings.HasSuffix(fileName, ".fbd") {
+			isPkgDir = true
+		}
+	}
+
+	if isPkgDir {
+		pkgName := strings.TrimPrefix(base, "fbd-")
+		pkg := Package{Name: pkgName, Path: pkgPath}
+		packages[pkgName] = append(packages[pkgName], &pkg)
+	}
 
 	return packages
 }
