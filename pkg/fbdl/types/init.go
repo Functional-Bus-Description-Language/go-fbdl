f796ad5a381414b4c648361ba4517b83417ed153
Michał Kruszewski <mkru@protonmail.com>
2025-10-16 12:46:17 +0200

    types: Add some doc

--- pkg/fbdl/types/init.go
+++ pkg/fbdl/types/init.go
@@ -3,6 +3,9 @@ package types
 var busWidth int64
 
 // bw is the busWidth.
+//
+// Having bus width as a global package variable helps keeping access make
+// functions easier to call, as all of them require information on bus width.
 func Init(bw int64) {
 	busWidth = bw
 }
